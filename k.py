e403e73cb69ec27d5745ffc15f670e06c9278f1a
智Aordatus <aordatus@gmail.com>
2022-11-15 16:23:31 +0530

    k complete

--- k.py
+++ k.py
@@ -1,31 +1,29 @@
 import math
-x=int(input())
-if(x%2==0):
-    y=int(x/2)
-else:
-    y=math.ceil(x/2)
-print(y)
+x = int(input())
+y = math.ceil(x/2)
+
 for row in range(y):
     for col in range(y+1):
         if col==0:
             print("*",end="")
         elif(col+row==y):
-            print("*",end="")
+            print("*")
+            break
         else:
             print(end=" ")
-        if col==y:
-            print()
+
 if(x%2==0):
     print("**")
-for row1 in range(y-1):
-    for col1 in range(y):
-        if col1==0:
+
+for row in range(y-1):
+    for col in range(y+1):
+        if col==0:
             print("*",end="")
-        elif(col1-row1==2):
-            print("*",end="")
+        elif(col-row==2):
+            print("*")
+            break
         else:
             print(end=" ")
-        if col1==y-1:
-            print()
+
 
 end = input()
